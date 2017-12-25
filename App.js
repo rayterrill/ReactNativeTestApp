2ee626b04db3e315d62ca6551c1389e08c76dc73
rayterrill <rayjohnterrill@gmail.com>
2017-12-25 16:33:53 -0500

    Added ability to play audio.

--- App.js
+++ App.js
@@ -1,23 +1,46 @@
 import React, { Component } from 'react';
-import { AppRegistry, View, Image } from 'react-native';
+import { Alert, AppRegistry, View, Image, ImageBackground, StyleSheet, Text, TouchableHighlight } from 'react-native';
+import { Audio } from 'expo';
 
 export default class AlignItemsBasics extends Component {
+  async playMusic() {
+	console.log('Trying to play music!');
+	const soundObject = new Expo.Audio.Sound();
+    try {
+      await soundObject.loadAsync(require('./cartoon_whoopee_cushion_air_release_accident.mp3'));
+	  await soundObject.playAsync();
+	} catch (error) {
+	  console.log('Error!');
+	}
+  }
+
+  _onPressButton() {
+	{this.playMusic()}
+  };
+
   render() {
     return (
-      // Try setting `alignItems` to 'flex-start'
-      // Try setting `justifyContent` to `flex-end`.
-      // Try setting `flexDirection` to `row`.
-      <View style={{
-        flex: 1,
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-      }}>
-        <Image source={require('./Timbers_Army_crest.png')} />
-      </View>
+      <ImageBackground
+        source={require('./sunburst.png')}
+        style={styles.container}>
+          <TouchableHighlight onPress={this._onPressButton.bind(this)}>
+             <Image source={require('./Timbers_Army_crest.png')} />
+          </TouchableHighlight>
+      </ImageBackground>
     );
-  }
+  };
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: undefined,
+    height: undefined,
+    backgroundColor:'transparent',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 // skip this line if using Create React Native App
 AppRegistry.registerComponent('AwesomeProject', () => AlignItemsBasics);
