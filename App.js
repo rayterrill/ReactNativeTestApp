5ffac1f50304159d3aa2814a4fa398cbe7187d35
rayterrill <rayjohnterrill@gmail.com>
2017-12-26 20:52:59 -0500

    Added layout with 2 columns and rows, added ability to play mp3s.

--- App.js
+++ App.js
@@ -3,19 +3,41 @@ import { Alert, AppRegistry, View, Image, ImageBackground, StyleSheet, Text, Tou
 import { Audio } from 'expo';
 
 export default class AlignItemsBasics extends Component {
-  async playMusic() {
-	console.log('Trying to play music!');
+  constructor(props) {
+    super(props);
+    this.state = {
+      musicPlaying: [],
+    }
+  }
+
+  async playMusic(item) {
 	const soundObject = new Expo.Audio.Sound();
     try {
-      await soundObject.loadAsync(require('./cartoon_whoopee_cushion_air_release_accident.mp3'));
+      if (item === 'Evening_Fall_Harp.mp3') {
+          await soundObject.loadAsync(require('./assets/music/Evening_Fall_Harp.mp3'));
+	  } else if (item === 'cartoon_whoopee_cushion_air_release_accident.mp3') {
+		  await soundObject.loadAsync(require('./assets/music/cartoon_whoopee_cushion_air_release_accident.mp3'));
+	  } else {
+		  await soundObject.loadAsync(require('./assets/music/cartoon_whoopee_cushion_air_release_accident.mp3'));
+	  }
+	  
+	  //stop the sound if something is already playing
+	  if (this.state.musicPlaying.length != 0) {
+        await this.state.musicPlaying.stopAsync();
+	  }
+
+	  //play the sound
 	  await soundObject.playAsync();
+	  
+	  //save the state of what's playing
+	  this.setState({musicPlaying: soundObject});
 	} catch (error) {
-	  console.log('Error!');
+	  console.log(error);
 	}
   }
 
-  _onPressButton() {
-	{this.playMusic()}
+  _onPressButton(item) {
+	{this.playMusic(item)}
   };
 
   render() {
@@ -23,9 +45,30 @@ export default class AlignItemsBasics extends Component {
       <ImageBackground
         source={require('./sunburst.png')}
         style={styles.container}>
-          <TouchableHighlight onPress={this._onPressButton.bind(this)}>
+		<View style={styles.row}>
+          <TouchableHighlight onPress={this._onPressButton.bind(this, 'Evening_Fall_Harp.mp3')} style={styles.item}>
              <Image source={require('./Timbers_Army_crest.png')} />
           </TouchableHighlight>
+          <TouchableHighlight onPress={this._onPressButton.bind(this, 'cartoon_whoopee_cushion_air_release_accident.mp3')} style={styles.item}>
+             <Image source={require('./Timbers_Army_crest.png')} />
+          </TouchableHighlight>
+		</View>
+		<View style={styles.row}>
+          <TouchableHighlight onPress={this._onPressButton.bind(this)} style={styles.item}>
+             <Image source={require('./Timbers_Army_crest.png')} />
+          </TouchableHighlight>
+          <TouchableHighlight onPress={this._onPressButton.bind(this)} style={styles.item}>
+             <Image source={require('./Timbers_Army_crest.png')} />
+          </TouchableHighlight>
+		</View>
+		<View style={styles.row}>
+          <TouchableHighlight onPress={this._onPressButton.bind(this)} style={styles.item}>
+             <Image source={require('./Timbers_Army_crest.png')} />
+          </TouchableHighlight>
+          <TouchableHighlight onPress={this._onPressButton.bind(this)} style={styles.item}>
+             <Image source={require('./Timbers_Army_crest.png')} />
+          </TouchableHighlight>
+		</View>
       </ImageBackground>
     );
   };
@@ -40,6 +83,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  row: {
+    flexDirection: 'row',
+  },
+  item: {
+	padding: 20,
+  }
 });
 
 // skip this line if using Create React Native App
